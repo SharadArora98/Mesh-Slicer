46c04fdfc89434c1212d0490d0ba41b460be58c7
schmoop <sarora@arkaenergy.com>
2026-04-11 18:11:14 +0530

    added orbit controls and cutmanager

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-import * as THREE from 'three.js';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import CutManager from './cutManager';
 
 const canvas = document.getElementById('designCanvas');
 const scene = new THREE.Scene();
@@ -8,15 +10,21 @@ camera.position.z = 2;
 const renderer = new THREE.WebGLRenderer({canvas, antialias:true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
 const geometry = new THREE.BoxGeometry(1,1,1);
 const material = new THREE.MeshBasicMaterial({color:0xff0000});
 
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+const cutManager = new CutManager(mesh, camera, renderer, controls, scene);
+cutManager.init();
 function animate() {
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
+    controls.update();
 }
 
 animate();
